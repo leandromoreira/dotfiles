0fbdb4c63af35408070b5eb4a6762e1762f316f3
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-13 01:26:45 -0300

    reorganizing the scripts for apt get

--- apt-get.sh
+++ apt-get.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo apt-get install git
 sudo apt-get install vim
-sudo apt-get install ack-grep
-sudo apt-get install curl
+sudo apt-get install ack-grep xclip curl build-essential openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
 
 #installing rvm
 curl -L get.rvm.io | bash -s stable
@@ -11,7 +10,5 @@ rvm reload
 rvm install 1.9.3
 rvm reload
 rvm use 1.9.3
-#general requirement for dev
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
-
 source ~/.rvm/scripts/rvm
+
