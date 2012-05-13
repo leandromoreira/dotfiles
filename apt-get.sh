d7eb4ef20358cf9929cde7b12d5258379dfdbe51
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-13 12:13:54 -0300

    now it has rhino as dependency and i also refactored (sic)

--- apt-get.sh
+++ apt-get.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-sudo apt-get install git
+sudo apt-get install git subversion
 sudo apt-get install vim-nox
-sudo apt-get install ack-grep xclip curl build-essential openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
+sudo apt-get install ack-grep xclip curl build-essential openssl 
+sudo apt-get install libreadline6 libreadline6-dev curl zlib1g zlib1g-dev 
+sudo apt-get install libssl-dev libyaml-dev libsqlite3-dev sqlite3 
+sudo apt-get install libxml2-dev libxslt-dev autoconf libc6-dev 
+sudo apt-get install ncurses-dev automake libtool bison gyp
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-jdk7-installer
 sudo apt-get install rhino
+
+if [ ! -d ~/.bin ];
+then
+  mkdir ~/.bin && cd ~/.bin
+else
+  cd ~/.bin
+fi
 
 #installing rvm
 curl -L get.rvm.io | bash -s stable
