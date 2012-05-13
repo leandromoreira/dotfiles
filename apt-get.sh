6ef9d4465999c8a3b26273fb91c49edcee777303
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-13 01:19:49 -0300

    added ruby and rvm to script

--- apt-get.sh
+++ apt-get.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 sudo apt-get install git
 sudo apt-get install vim
-sudo apt-get install openssl
 sudo apt-get install ack-grep
+sudo apt-get install curl
 
+#installing rvm
+curl -L get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm reload 
+rvm install 1.9.3
+rvm reload
+rvm use 1.9.3
+#general requirement for dev
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
+
+source ~/.rvm/scripts/rvm
