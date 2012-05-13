b8b32b08df6213407e9542287b56738822db3c6f
Your Name Comes Here <you@yourdomain.example.com>
2012-05-13 00:20:05 -0300

    initial messy for dotfiles

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+
+#config git for me
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global user.name "Your Name Comes Here"
+git config --global user.email you@yourdomain.example.com
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+git config --global core.editor "vim"
