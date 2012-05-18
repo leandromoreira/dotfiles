beab03688b5071e1f79abd4ced3d020a0a54f078
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-17 22:01:46 -0300

    now my gitconfig are available

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ then
   ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi
 
+if [ ! -f ~/.gitconfig ];
+then
+  ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+fi
+
 if [ ! -d ~/.vim ];
 then
   ln -s ~/dotfiles/.vim ~/.vim
@@ -20,16 +25,3 @@ fi
 
 export PATH=$PATH:"~/bin/"
 
-#config git for me
-git config --global alias.st status
-git config --global alias.l log
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global user.name "Leandro Moreira"
-git config --global user.email leandro.ribeiro.moreira@gmail.com 
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-git config --global core.editor "vim"
