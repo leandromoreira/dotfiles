07f7563aaa5fa6256dbf820d02d094bea687e9e1
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-09-05 19:07:31 -0300

    add script for install and remove my config for a quick vim usage

---
+++ install_my_vim.sh
@@ -0,0 +1,49 @@
+sufix=".old99l"
+
+create_file_backup(){
+  mv $1 $1$sufix
+}
+
+create_dir_backup(){
+  mv -r $1 $1$sufix
+}
+
+if [ ! -f ~/.vimrc ];
+then
+  ln -s ~/dotfiles/.vimrc ~/.vimrc
+else
+  create_file_backup ~/.vimrc
+  ln -s ~/dotfiles/.vimrc ~/.vimrc
+fi
+
+if [ ! -d ~/.vim ];
+then
+  ln -s ~/dotfiles/.vim ~/.vim
+else
+  create_dir_backup ~/.vim
+  ln -s ~/dotfiles/.vim ~/.vim
+fi
+
+if [ ! -d ~/dotfiles/.vim/tmp ];
+then
+  mkdir ~/dotfiles/.vim/tmp
+fi
+
+if [ ! -d ~/dotfiles/.vim/backup ];
+then
+  mkdir ~/dotfiles/.vim/backup
+fi
+
+git submodule init
+git submodule update
+
+os=`uname`
+
+if [[ "$os" == 'Linux' ]]; then
+  . ~/dotfiles/linux_aliases
+elif [[ "$os" == 'Darwin' ]]; then
+  . ~/dotfiles/mac_aliases
+  export CLICOLOR=1
+fi
+. ~/dotfiles/common_aliases
+. ~/dotfiles/dev-bash-git-ps1/bash_git_ps1.sh
---
+++ uninstall_my_vim.sh
@@ -0,0 +1,25 @@
+sufix=".old99l"
+
+create_file_backup(){
+  mv $1$sufix $1
+}
+
+create_dir_backup(){
+  mv -r $1$sufix $1
+}
+
+if [ -f ~/.vimrc$sufix ];
+then
+  rm ~/.vimrc
+  create_file_backup ~/.vimrc
+fi
+
+if [ -d ~/.vim$sufix ];
+then
+  rm -rf ~/.vim
+  create_dir_backup ~/.vim
+fi
+
+echo 'Bye, bye (press any key to logout from this session)'
+read
+exit
