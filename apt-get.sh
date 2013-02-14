c41cb8cf7e922f7390cff640a1893d7ae8b620e0
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-02-14 00:40:39 -0200

    add apache benchmark ab and skips confirmation while installing

--- apt-get.sh
+++ apt-get.sh
@@ -3,16 +3,17 @@
 #java from oracle
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-jdk7-installer
+sudo apt-get install -y oracle-jdk7-installer
 
 #general tools
-sudo apt-get install git subversion
-sudo apt-get install vim-nox exuberant-ctags 
-sudo apt-get install ack-grep xclip curl build-essential openssl libpq-dev libcurl4-openssl-dev
-sudo apt-get install libreadline6 libreadline6-dev curl zlib1g zlib1g-dev 
-sudo apt-get install libssl-dev libyaml-dev libsqlite3-dev sqlite3 
-sudo apt-get install libxml2-dev libxslt-dev autoconf libc6-dev fortune
-sudo apt-get install ncurses-dev automake libtool bison gyp tree htop
-sudo apt-get install rhino
-sudo apt-get install nodejs
+sudo apt-get install -y git subversion
+sudo apt-get install -y vim-nox exuberant-ctags 
+sudo apt-get install -y ack-grep xclip curl build-essential openssl libpq-dev libcurl4-openssl-dev
+sudo apt-get install -y libreadline6 libreadline6-dev curl zlib1g zlib1g-dev 
+sudo apt-get install -y libssl-dev libyaml-dev libsqlite3-dev sqlite3 
+sudo apt-get install -y libxml2-dev libxslt-dev autoconf libc6-dev fortune
+sudo apt-get install -y ncurses-dev automake libtool bison gyp tree htop
+sudo apt-get install -y apache2-utils
+sudo apt-get install -y rhino
+sudo apt-get install -y nodejs
 
