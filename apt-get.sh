82f2021c67f3ac9fd433d0d54533ecc4d20941ba
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-03-09 11:15:23 -0300

    add the great tool httperf

--- apt-get.sh
+++ apt-get.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y libreadline6 libreadline6-dev curl zlib1g zlib1g-dev
 sudo apt-get install -y libssl-dev libyaml-dev libsqlite3-dev sqlite3 
 sudo apt-get install -y libxml2-dev libxslt-dev autoconf libc6-dev fortune
 sudo apt-get install -y ncurses-dev automake libtool bison gyp tree htop
-sudo apt-get install -y apache2-utils
+sudo apt-get install -y apache2-utils httperf
 sudo apt-get install -y rhino
 sudo apt-get install -y nodejs
 
