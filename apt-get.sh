d1c15084ea51756d751997d24edbd8ecf875f2df
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-03-29 18:41:37 -0300

    adds screen for my terminal

--- apt-get.sh
+++ apt-get.sh
@@ -7,10 +7,11 @@ sudo apt-get install -y oracle-jdk7-installer
 
 #general tools
 sudo apt-get install -y git subversion
-sudo apt-get install -y vim-nox exuberant-ctags 
+sudo apt-get install -y vim-nox exuberant-ctags
+sudo apt-get install -y screen
 sudo apt-get install -y ack-grep xclip curl build-essential openssl libpq-dev libcurl4-openssl-dev
-sudo apt-get install -y libreadline6 libreadline6-dev curl zlib1g zlib1g-dev 
-sudo apt-get install -y libssl-dev libyaml-dev libsqlite3-dev sqlite3 
+sudo apt-get install -y libreadline6 libreadline6-dev curl zlib1g zlib1g-dev
+sudo apt-get install -y libssl-dev libyaml-dev libsqlite3-dev sqlite3
 sudo apt-get install -y libxml2-dev libxslt-dev autoconf libc6-dev fortune
 sudo apt-get install -y ncurses-dev automake libtool bison gyp tree htop
 sudo apt-get install -y apache2-utils httperf
