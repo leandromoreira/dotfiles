233d6ffc3b1bbd1e4657d3cc62f0cdebe2d4fbef
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-03-29 21:45:05 -0300

    after a little research I changed my mind, Ill go with tmux

--- apt-get.sh
+++ apt-get.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y oracle-jdk7-installer
 #general tools
 sudo apt-get install -y git subversion
 sudo apt-get install -y vim-nox exuberant-ctags
-sudo apt-get install -y screen
+sudo apt-get install -y tmux
 sudo apt-get install -y ack-grep xclip curl build-essential openssl libpq-dev libcurl4-openssl-dev
 sudo apt-get install -y libreadline6 libreadline6-dev curl zlib1g zlib1g-dev
 sudo apt-get install -y libssl-dev libyaml-dev libsqlite3-dev sqlite3
