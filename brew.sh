11c6631b21ccbfb2712bb678c5c17f540d545129
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-04-08 10:38:56 -0300

    adds zsh as a shell option

--- brew.sh
+++ brew.sh
@@ -1,5 +1,6 @@
 brew install git bash-completion
 brew install macvim
+brew install zsh
 brew install ctags
 # fix for mac use this as its http://mattpolito.info/post/1648956809/ctags-got-you-down
 brew install fortune
