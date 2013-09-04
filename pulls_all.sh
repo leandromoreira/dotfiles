adb5c325402ccfc9cebf326847c2341eacde2552
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-09-04 11:21:26 -0300

    adds scrit that perform git pull for each sub directory

---
+++ pulls_all.sh
@@ -0,0 +1,9 @@
+if [ "$#" -ne 1 ] ; then
+  echo "###### Missing folder argument ######" >&2
+  echo " Usage: ./pulls_all.sh ~/git_projects/" >&2
+  echo "#####################################" >&2
+  exit 1
+fi
+
+
+for dir in `ls -d $1/*`; do (cd "$dir" && echo "########## $dir ##########" && git pull --rebase origin master); done
