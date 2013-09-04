5ce3993109f5596406ac12b6704e1d5f05dfa354
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2013-09-04 14:03:21 -0300

    better example for usage

--- pulls_all.sh
+++ pulls_all.sh
@@ -1,6 +1,6 @@
 if [ "$#" -ne 1 ] ; then
   echo "###### Missing folder argument ######" >&2
-  echo " Usage: ./pulls_all.sh ~/git_projects/" >&2
+  echo " Usage: ./pulls_all.sh ~/git_projects" >&2
   echo "#####################################" >&2
   exit 1
 fi
